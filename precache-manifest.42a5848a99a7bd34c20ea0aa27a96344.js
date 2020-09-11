3676bbd2eb66677864f4c83c656cdac687299fb0
Liviu Cauia <liviu.cauia@gmail.com>
2020-09-11 12:06:08 +0300

    Updates

---
+++ precache-manifest.42a5848a99a7bd34c20ea0aa27a96344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3839857c8dbce5c16d55d7ca1dd01488",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "42253bdab51793e98a7d",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f59f41d9810a3890d972",
+    "url": "/face-recognition-brain/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "42253bdab51793e98a7d",
+    "url": "/face-recognition-brain/static/js/2.de87b492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.de87b492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59f41d9810a3890d972",
+    "url": "/face-recognition-brain/static/js/main.2cbfd8de.chunk.js"
+  },
+  {
+    "revision": "2017eaf3b039845222aa",
+    "url": "/face-recognition-brain/static/js/runtime-main.39b17a26.js"
+  }
+]);
